33ece8a5e4dabd7b78481b2d2101d78057c084a6
Taylar Masson <tay.j.masson@gmail.com>
2015-08-28 13:52:02 -0700

    Starting Development

---
+++ src/main/java/com/tayjay/energyoverload/EnergyOverload.java
@@ -0,0 +1,35 @@
+package com.tayjay.energyoverload;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by Taylar on 28/08/2015.
+ */
+
+@Mod(modid = "EnergyOverload", name = "Energy Overload", version = "1.7.10-0.1")
+public class EnergyOverload
+{
+    @Mod.Instance("EnergyOverload")
+    public static EnergyOverload instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
---
+++ src/main/java/com/tayjay/energyoverload/proxy/ServerProxy.java
@@ -0,0 +1,7 @@
+package com.tayjay.energyoverload.proxy;
+
+/**
+ * Created by Taylar on 28/08/2015.
+ */
+public class ServerProxy extends CommonProxy{
+}
